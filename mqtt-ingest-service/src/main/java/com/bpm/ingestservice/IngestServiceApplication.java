35e52f052cfb57e4c3832dfec5c47186ef2fba4b
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-11-14 16:34:23 +0100

    first commit

---
+++ mqtt-ingest-service/src/main/java/com/bpm/ingestservice/IngestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bpm.ingestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestServiceApplication.class, args);
+    }
+
+}
