3d7795fd05e4c8910be4849fec677bf1a4a06044
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-11-17 14:02:35 +0100

    added measurement-query-service

---
+++ measurement-query-service/src/main/java/com/bpm/measurementqueryservice/MeasurementQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bpm.measurementqueryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementQueryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeasurementQueryServiceApplication.class, args);
+    }
+
+}
---
+++ measurement-query-service/src/test/java/com/bpm/measurementqueryservice/MeasurementQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpm.measurementqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
