2f27ea95494ff7a1e62fba21eafae48727bb09ee
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-11-25 10:10:15 +0100

    added application properties for tests in config, discovery and gateway services

--- discovery-service/src/test/java/com/bpm/discoveryservice/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/com/bpm/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.bpm.discoveryservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration.class
+})
 class DiscoveryServiceApplicationTests {
 
     @Test
